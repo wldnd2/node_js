e4b53eedd88bfdbf982e4e6c1eb80d4db4edd0f5
wldnd2 <jiwoong000628@gmail.com>
2023-07-10 14:10:00 +0530

    Feat: 각클래스 출력 구현

--- YOLO8.py
+++ YOLO8.py
@@ -12,8 +12,14 @@ results = []
 for image_file in image_files:
     with Image.open(image_file) as img:
         img_size = img.size
-    # res = model(image_file,conf=0.5)
+    res = model(image_file,conf=0.5)
     # source에 이미지 url주소도 가능
-    res = model.predict(source=image_file, save=True, save_txt=True)
+    # res = model.predict(source=image_file, save=True, save_txt=True)
+    names = model.names
+    for r in res:
+        print(r.boxes.cls)
+        print("*************")
+        for c in r.boxes.cls:
+            print(names[int(c)])
     resized_img = cv2.resize(res[0].plot(), (img.size[0], img.size[1]))
     cv2.imwrite("./result/YOLO8_temp.jpg", resized_img)
